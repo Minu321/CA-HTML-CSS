56e661c1de96221c5705fca05bbb5308026ab8d9
Mina <mina_rb@outlook.com>
2023-09-07 20:42:37 +0200

    Added description and title from api

--- js/index.js
+++ js/index.js
@@ -6,11 +6,21 @@ fetchProducts()
   .then((productData) => {
     console.log("Fetched product data:", productData);
 
+    const titleElement = document.querySelectorAll(".shopitem h2#title");
     const priceElements = document.querySelectorAll(".shopitem p#price");
+    const descriptionElement = document.querySelectorAll(
+      ".shopitem p#description"
+    );
 
     productData.forEach((product, index) => {
+      const currentTitleElement = titleElement[index];
+      currentTitleElement.textContent = product.title;
+
       const currentPriceElement = priceElements[index];
       currentPriceElement.textContent = `${product.price}$`;
+
+      const currenDescriptionElement = descriptionElement[index];
+      currenDescriptionElement.textContent = product.description;
     });
   })
   .catch((error) => {
